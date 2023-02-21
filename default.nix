e24122edbbcc276bf2cfeca923586de16c9ec42e
salman <salman@ssddq.net>
2023-02-20 19:13:28 -0500

    support building as a flake

--- default.nix
+++ default.nix
@@ -1,11 +1,12 @@
-let
-  pkgs = import <nixpkgs> { };
-in 
+{ pkgs ? import <nixpkgs> {} , ecpath ? "./EmbeddedController" }:
+
 pkgs.stdenv.mkDerivation {
   pname   = "fw-ectool";
   version = "0.0.16715";
 
   src = ./.;
+
+  ecpath = ecpath;
 
   buildInputs = [
     pkgs.pkg-config
@@ -15,10 +16,23 @@ pkgs.stdenv.mkDerivation {
   ];
 
   configurePhase = ''
-    cp -r $src/* .
-    cd EmbeddedController
+    # Flakes do not normally copy submodules when building.
+    # As a workaround, the submodule needs to be copied from
+    # the path passed to the derivation from the flake.
+    # The local copy of the submodule is used if the submodule is present,
+    # checked by the presence of the README.md file;
+    # otherwise, nix build uses a copy of the Github repository
+    # that was downloaded to the store.
+
+    if [ ! -f "./EmbeddedController/README.md" ]
+    then
+      mkdir -p ./EmbeddedController
+      mkdir -p ./EmbeddedController/util
+      cp -r $ecpath/* ./EmbeddedController
+    fi
+    cd ./EmbeddedController
     patchShebangs ./util/getversion.sh
-    bash util/getversion.sh > util/ec_version.h
+    bash ./util/getversion.sh > ./util/ec_version.h
   '';
 
   buildPhase = ''
